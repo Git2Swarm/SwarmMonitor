e465e7ccfa6a79c2f958b91c7bcdb5b61c9c4454
Jeeva <jeeva.chelladhurai@in.ibm.com>
2016-05-23 09:30:53 -0500

    performance improvement, removed node-json2html dependency, beautification

--- dmonweb/src/dmonweb.js
+++ dmonweb/src/dmonweb.js
@@ -1,69 +1,71 @@
-var json2html = require('node-json2html');
 var fs = require("fs");
 var http = require('http');
 
 var filename = "/data/cont-stats.json";
 
-var metatag = {'tag':'meta http-equiv="refresh" content="15"', 'html':''};
-
 http.createServer(function(request,response){
+
+    var data = fs.readFileSync(filename);
+    var jsondata = JSON.parse(data);
+    var tableData = "";
+
+    var totCpuUsage = 0;
+    var totMemUsage = 0;
+    var totNetUsage = 0;
+    var totdskUsage = 0;
+
+    for(var i=0;i<jsondata.length;i++) {
+        var cpuUsage = parseInt(jsondata[i].precpu_stats.cpu_usage.total_usage);
+        var memUsage = parseInt(jsondata[i].memory_stats.usage);
+        var netUsage = parseInt(jsondata[i].networks.eth0.rx_bytes) +
+                       parseInt(jsondata[i].networks.eth0.tx_bytes);
+        var dskUsage = parseInt(jsondata[i].blkio_stats.io_service_bytes_recursive[4] == undefined ? 0 :
+                             jsondata[i].blkio_stats.io_service_bytes_recursive[4].value);
+
+        tableData += ("<tr>" +
+                       "<td> " + jsondata[i].Name + "</td>" +
+                       "<td style='text-align: right;padding-left:25px; padding-right:25px;'>" + cpuUsage + "</td>" +
+                       "<td style='text-align: right;padding-left:25px; padding-right:25px;'>" + memUsage + "</td>" +
+                       "<td style='text-align: right;padding-left:25px; padding-right:25px;'>" + netUsage + "</td>" +
+                       "<td style='text-align: right;padding-left:25px; padding-right:25px;'>" + dskUsage + "</td>" +
+                     "</tr>");
+
+
+        totCpuUsage += cpuUsage;
+        totMemUsage += memUsage;
+        totNetUsage += netUsage;
+        totdskUsage += dskUsage;
+
+    }
+
+    var tableHeader = ("<tr style='background-color: #c0c0c0;'>" +
+                            "<th style='text-align: center;'></th>" +
+                            "<th style='text-align: center;'> CPU</br>"+ totCpuUsage +"</th>"+
+                            "<th style='text-align: center;'> Memory</br>"+ totMemUsage +"</th>"+
+                            "<th style='text-align: center;'> NET I/O</br>"+ totNetUsage +"</th>"+
+                            "<th style='text-align: center;'> BLOCK I/O</br>"+ totdskUsage +"</th>"+
+                        "</tr>" );
+
     response.writeHead(200,{'Content-Type':'text/html'});
 
-     var data = fs.readFileSync(filename);
-     var jsondata = JSON.parse(data);
-     
-     var cpuUsage = 0;
-     var memUsage = 0;
-     var netUsage = 0;
-     var dskUsage = 0;
-
-       
-    for(var i=0;i<jsondata.length;i++) {
-        cpuUsage += parseInt(jsondata[i].precpu_stats.cpu_usage.total_usage);
-        
-    } 
-  
-    for(var i=0;i<jsondata.length;i++) {
-        memUsage += parseInt(jsondata[i].memory_stats.usage);
-        
-    }
-
-    for(var i=0;i<jsondata.length;i++) {
-        netUsage = netUsage +
-                   parseInt(jsondata[i].networks.eth0.rx_bytes) +
-                   parseInt(jsondata[i].networks.eth0.tx_bytes);
-        
-    }
-  
-    for(var i=0;i<jsondata.length;i++) {
-        dskUsage += parseInt(jsondata[i].blkio_stats.io_service_bytes_recursive[4] == undefined ? 0 :
-                             jsondata[i].blkio_stats.io_service_bytes_recursive[4].value); 
-    }
- 
-    var tablecontent={"tag":"tr","children":[{"tag":"th style='visibility:hidden;'","html":""},
-                {"tag":"th style='text-align: center;'","html":"CPU </br>"       + cpuUsage},
-                {"tag":"th style='text-align: center;'","html":"Memory </br>"    + memUsage},
-                {"tag":"th style='text-align: center;'","html":"NET I/O </br>"   + netUsage},
-                {"tag":"th style='text-align: center;'","html":"BLOCK I/O </br>" + dskUsage}]};
-
-    var tabledata ={"tag":"tr","children":[
-                {"tag":"td style='padding-left:25px; padding-right:25px;'","html":"${Name}"},
-                {"tag":"td style='text-align: right;padding-left:25px; padding-right:25px;'","html":"${precpu_stats.cpu_usage.total_usage}"},
-                {"tag":"td style='text-align: right;padding-left:25px; padding-right:25px;'","html":"${memory_stats.usage}"},
-                {"tag":"td style='text-align: right; padding-left:25px; padding-right:25px;'","html":"${networks.eth0.rx_bytes}"},
-                {"tag":"td style='text-align: right; padding-left:25px; padding-right:25px;'","html":"${networks.eth0.tx_bytes}"}]};
-    
- 
-    response.write(json2html.transform({},metatag));
-    response.write("<table style='background-color:LightGray; float: center; padding:25px 25px 25px 25px;'>");
+    response.write("<html> \
+                      <head> \
+                        <meta http-equiv='refresh' content='5'>  \
+                        <title>Swarm Monitor</title> \
+                        <style>tr:nth-child(odd) {background: #ffffff;} tr:nth-child(even) { background: #c0c0c0;}</style> \
+                      </head> \
+                      <body>");
+    response.write("<table style='float:center; padding:25px 25px 25px 25px;'>"); 
     response.write("<thead><bold>");
-    response.write(json2html.transform({},tablecontent));
+    response.write(tableHeader);
     response.write("</bold></thead>");
     response.write("<tbody>");
-    response.write(json2html.transform(jsondata,tabledata));
+    response.write(tableData);
     response.write("</tbody>");
     response.write("</table>");
+    response.write("</body></html>");
     response.end();
 }).listen(5000);
+
 console.log("Server listening to port : 5000");
 
